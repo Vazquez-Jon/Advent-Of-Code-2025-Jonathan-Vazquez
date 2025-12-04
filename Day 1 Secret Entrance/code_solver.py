929602bccf96993d52769a065a4c7c8c334e2b2a
JonathanVUCD <j0n4than9100@gmail.com>
2025-12-03 20:45:29 -0800

    main

---
+++ Day 1 Secret Entrance/code_solver.py
@@ -0,0 +1,12 @@
+# Jonathan Vazquez
+
+
+filepath = './Input.txt'
+
+def main():
+    print('Hello')
+
+    with open(filepath, 'r') as file:
+        for line in file:
+            int(file[1:])
+            
