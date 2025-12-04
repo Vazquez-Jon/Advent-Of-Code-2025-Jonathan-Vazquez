8c96b8722f5d100049878d9a26722d83874ad433
JonathanVUCD <j0n4than9100@gmail.com>
2025-12-03 20:49:19 -0800

    function to tell if letter is positive

--- Day 1 Secret Entrance/code_solver.py
+++ Day 1 Secret Entrance/code_solver.py
@@ -3,10 +3,16 @@
 
 filepath = './Input.txt'
 
+def negOrPosLetter(turnDirection):
+    if (turnDirection == "R"):
+        return True
+    return False
+
 def main():
     print('Hello')
 
+    code = 0
+    lock = 50
     with open(filepath, 'r') as file:
         for line in file:
             int(file[1:])
-            
