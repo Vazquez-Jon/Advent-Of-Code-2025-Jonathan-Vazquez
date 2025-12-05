bd44b983911ccf9d09ead1fc042cd60e726535b1
JonathanVUCD <j0n4than9100@gmail.com>
2025-12-04 20:58:00 -0800

    updateLock functionality

--- Day 1 Secret Entrance/code_solver.py
+++ Day 1 Secret Entrance/code_solver.py
@@ -3,10 +3,28 @@
 
 filepath = './Input.txt'
 
-def negOrPosLetter(turnDirection):
+def posLetter(turnDirection: str) -> bool:
     if (turnDirection == "R"):
         return True
     return False
+
+def updateLock(lock: int, dir: str, turns: int) -> int:
+    resets = 0
+    if (dir == "R") :
+        lock += turns 
+    else:
+        lock -= turns
+
+    if (lock > 99):
+        lock = lock % 100
+        resets += 1
+    elif (lock < 0):
+        lock = abs(lock)
+    elif (lock < -99):
+        lock = 100 - (abs(lock) - 100)
+        resets += 1 
+    
+    return [lock, resets]
 
 def main():
     print('Hello')
@@ -15,4 +33,5 @@ def main():
     lock = 50
     with open(filepath, 'r') as file:
         for line in file:
-            int(file[1:])
+            turns = int(line[1:])
+            dir   = line[:1]
